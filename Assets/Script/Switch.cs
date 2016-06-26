2cf004a895f6c0217db7b0756171acd785c69b5b
parkjonghwa <pjh2455191@naver.com>
2016-06-26 12:23:05 +0900

    박종화
    수정 저장

---
+++ Assets/Script/Switch.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Switch : MonoBehaviour {
+
+    public GameObject switch2;
+    Vector3 pos_switch2;
+    public float distance;
+    public bool on = false;
+    public bool check = false;
+    public enum DIRECTION {
+        UP,
+        LEFT,
+        RIGHT,
+        DOWN
+    }
+    public DIRECTION direc;
+	void Start ()
+    {
+        pos_switch2 = switch2.gameObject.transform.localPosition;
+	}
+    void switch_on()
+    {
+        check = false;
+        if (direc == DIRECTION.UP)
+            switch2.transform.localPosition += Vector3.up * distance;
+        else if (direc == DIRECTION.DOWN)
+            switch2.transform.localPosition += Vector3.down * distance;
+        else if (direc == DIRECTION.RIGHT)
+            switch2.transform.localPosition += Vector3.right * distance;
+        else
+            switch2.transform.localPosition += Vector3.left * distance;
+    }
+    void switch_off()
+    {
+        switch2.transform.localPosition = pos_switch2;
+    }
+	void Update ()
+    {
+        if (on == true && check == true)
+            switch_on();
+        else if (on == false && check == true)
+            switch_off();
+	    
+	}
+}
